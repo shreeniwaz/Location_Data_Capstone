16234c4889177775ce66a54b82ff02282bec13bb
shreeniwaz <shreeniwaz@gmail.com>
2021-01-29 17:48:52 +0530

    Add files via upload

---
+++ Capstone Course 1st Assignment.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Capstone Project: 1st Assignment</h1>
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
